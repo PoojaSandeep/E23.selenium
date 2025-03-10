6437e8b824c1961d8228a1e68d22d47e5baabdd3
sandeep <sandeep@SandeePooja.lan>
2025-03-09 23:20:38 -0400

    Committing Selenium changes

--- src/main/java/testNG/AssertionsPractice.java
+++ src/main/java/testNG/AssertionsPractice.java
@@ -1,5 +1,30 @@
 package testNG;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class AssertionsPractice {
+	
+	@Test
+	//Run | Debug
+	public void demoTest()
+	{
+		String a="hello";
+		SoftAssert sa=new SoftAssert();
+		
+		System.out.println("Step 1");
+		System.out.println("Step 2");
+		
+		Assert.assertEquals(0, 1);//fail
+		
+		System.out.println("step 3");
+		System.out.println("step 4");
+		
+		Assert.assertEquals(1, 0);
+		System.out.println("step 5");
+		
+		sa.assertAll();
+	}
 
 }
